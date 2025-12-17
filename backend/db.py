c6f578235bb322bc7f9c14534a3ad868f911e9aa
Sanchay Singh <sanchaysingh21@gmail.com>
2025-12-17 12:22:11 +0530

    Connected mongoDB

---
+++ backend/db.py
@@ -0,0 +1,12 @@
+import os 
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client["playlify"]
+
+profile_col = db["profile"]
