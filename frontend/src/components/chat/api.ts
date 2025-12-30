6a64254d4a6334f98597caa8c79e3f85d3a2a0c4
Sanchay Singh <sanchaysingh21@gmail.com>
2025-12-31 00:22:18 +0530

    fetching intent from frontend

--- frontend/src/components/chat/api.ts
+++ frontend/src/components/chat/api.ts
@@ -9,13 +9,14 @@ export const sendTextMessage = async (message: string) => {
      );
 
     const songList: string[] = response.data?.songs?.songs || [];
+    const responseText = response.data?.response || "";
 
     return {
       text: songList.length
         ? `🎵 Here's a playlist for you:\n\n${songList
             .map((song, idx) => `${idx + 1}. ${song}`)
             .join("\n")}`
-        : null,
+        : responseText,
 
       raw: response.data,
     };
